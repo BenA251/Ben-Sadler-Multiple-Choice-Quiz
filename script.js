236740addf7cb345084f0d516454c63581223292
B251 <ba.sadler1@gmail.com>
2023-12-29 10:43:40 +0000

    minor script additions

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log (document);
+var time = document.querySelector("#time");
+console.log(time.textContent)
+time.textContent = "45"
+
+var startButton = document.querySelector("#start");
+startButton.click();
